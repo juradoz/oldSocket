14f452e3bdf0138f68c4551ac45e98d8625aa950
Daniel Jurado <jurado@gmail.com>
2010-04-27 20:20:34 -0300

    Acrescentei alguns metodos na interface.

--- src/br/com/gennex/interfaces/TcpController.java
+++ src/br/com/gennex/interfaces/TcpController.java
@@ -3,5 +3,9 @@ package br.com.gennex.interfaces;
 public interface TcpController {
 	void addHandler(TcpRequest request, TcpRequestHandler requestHandler);
 
+	void clearHandlers();
+
+	TcpRequestHandler removeHandler(TcpRequest request);
+
 	TcpResponse processRequest(TcpRequestCommand request);
 }
